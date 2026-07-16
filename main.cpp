a5a297fa326eafc9b5d32d7490232b38bb0d9fe6
maxmullerbueno <max.albieri@outlook.com>
2026-07-16 04:38:27 +0000

    feat: class private and public

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+class RandomNumber
+{
+private:
+    int number;
+    
+public:
+    void generate()
+    {
+
+    void display()
+    {
+        cout << "Random Number: " << this->number << endl;
+    }
+};
